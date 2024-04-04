805484dc532d05fe4638235c424086f0c9200ee7
Rieser, Alix <111894791+riesera@users.noreply.github.com>
2024-04-04 13:35:59 -0500

    Fully Functional V1.js
    All variables added and function correctly, text message could still be formatted better.

--- web_work/autoSMS.js
+++ web_work/autoSMS.js
@@ -1,10 +1,54 @@
 widget.on("submit", () => {
     
-    //Let's define the form variables here for use later
-    const zipCode = document.getElementById("lsm27551").value;
-    console.log("The zipcode is ", zipCode);
-    const smsMessage = "Hi I live in " + zipCode; //Craft the message
+    //Form variables
+    const name = document.getElementById("62ce755cf34f4").value; //Text Box
+    const zipCode = document.getElementById("lsm27551").value; //Text Box
+    const tvSize = document.getElementById("lriepv3l"); //Drop Down
+    const needMount = document.getElementById("lrif6rcb"); //Drop Down
+    const elevated = document.getElementById("lries6jl"); //Drop Down
+    const outlet = document.getElementById("lu7okerx"); //Drop Down W/ Conditional
+    //Conditional if outlet == No
+    let describeElectric = "";
+    let describeElectricMessage = "";
+    let newLine = "";
+    if (outlet.options[outlet.selectedIndex].text == "No"){
+        describeElectric = document.getElementById("lu7op5yx").value; //Text Box
+        describeElectricMessage = "Electric Description: ";
+        newLine = "\n";
+    }
+    //Optional Check Boxes
+    const otherServicesCheck = document.getElementById("lrifhpq1"); //Check Box
+    let otherServices = "";
+    if (otherServicesCheck.checked){
+        otherServices = "I am interested in additional services. \n";
+    }
+    const sameDay = document.getElementById("62ce755cf3b96"); //Check Box
+    //Conditional if sameDay is checked
+    let tvModel = "";
+    let sameDayMessage = "";
+    if(sameDay.checked){
+        tvModel = document.getElementById("lu7p0q1w").value; //Text Box
+        sameDayMessage = "Yes, I am interested in same day delivery with price match." +
+        "\nTV Model & Website: ";
+        newLine = "\n";
+    }
+    const specialMessage = document.getElementById("62ce755cf3b57").value; //Text Box
+    
+    //SMS Message
+    const smsMessage = "Hi, I would like a quote. \n" + 
+    "Name: " + name + " \n" + 
+    "Zipcode: " + zipCode + "\n" + 
+    "TV Size: " + tvSize.options[tvSize.selectedIndex].text + "\n" +
+    "Mount: " + needMount.options[needMount.selectedIndex].text + "\n" +
+    "Mounting Elevated: " + elevated.options[elevated.selectedIndex].text + "\n" +
+    "Outlet: " + outlet.options[outlet.selectedIndex].text + "\n" + 
+    describeElectricMessage + describeElectric + newLine + 
+    otherServices + 
+    sameDayMessage + tvModel + newLine + specialMessage; 
+    
     console.log(smsMessage);
+    
+    //Link to the message
     const smsLink = "sms:xxxxxxxxxx?&body=" + encodeURIComponent(smsMessage);
 
     //Define the waitForElement function
@@ -40,3 +84,4 @@ widget.on("submit", () => {
     });
   
 });
+
